62703c74351fb937ac0e322b711e04b22ba8c10a
gaofeixiang <gaofeixiang@yuanfudao.com>
2021-09-02 19:11:24 +0800

    修改insert命令

--- routes/ada-school-ide.js
+++ routes/ada-school-ide.js
@@ -9,7 +9,7 @@ const connection = require('../modules/ada-school-ide')
 
 // 新增异常
 router.post('/', (req, res, next) => {
-  connection.query('INSERT INTO exception(source, type, file) VALUES(1, 2, 3)', {}, (err, result) => {
+  connection.query('INSERT INTO exception(workId, userId, tenantId ) VALUES(1, 2, 3)', {}, (err, result) => {
     console.log('req', req)
     console.log('err', err)
     console.log('result', result)
